04ccf66be4449242d5458c4cc3720a8bf053f427
Gabriel Bauer <gabeb1277@gmail.com>
2025-05-06 20:27:25 -0400

    add directory.rs

---
+++ hints/directory.rs
@@ -0,0 +1,14 @@
+use arrayvec::ArrayString;
+const DIR_NAME: u16 = 48;
+/*
+typedef struct dirent {
+    char name[DIR_NAME];
+    int  inum;
+    char _reserved[12];
+} dirent;
+*/
+
+struct Dirent {
+	name: ArrayString<DIR_NAME>;
+	inum: u16;
+}
