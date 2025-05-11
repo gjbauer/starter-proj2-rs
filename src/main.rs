c9cfb8e46582f4e6fcd995b482e39a3de8d240e6
Gabriel Bauer <gabeb1277@gmail.com>
2025-05-11 11:31:08 -0400

    getattr

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 extern crate fuse;
+extern crate libc;
+extern crate time;
 
 use std::env;
-use fuse::Filesystem;
+use std::path::Path;
+use std::slice;
+use libc::{ENOENT, ENOSYS};
+use fuse::{FileAttr, FileType, Filesystem, Request, ReplyAttr, ReplyData, ReplyEntry, ReplyDirectory};
+mod inode;
+mod directory;
+mod disk;
+mod hash;
 
 struct Nufs;
 
 impl Filesystem for Nufs {
 	// TODO: Put prototype functions here...
+	fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
+		println!("getattr(ino={})", ino);
+		reply.error(ENOSYS);
+	}
 }
 
 fn main() {
