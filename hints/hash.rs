48930ebe29499a872fbba87546ca90ed20faaf88
Gabriel Bauer <gabeb1277@gmail.com>
2025-05-11 11:36:00 -0400

    rm hash interior

--- hints/hash.rs
+++ hints/hash.rs
@@ -1,13 +1,5 @@
 use crate::directory;
 
 pub fn hash(name: [char; directory::DIR_NAME]) -> usize {
-	let mut hash = 5381;
-	let mut c: usize;
-	
-	for i in 0..=directory::DIR_NAME-1 {
-		c = name[i] as usize;
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	
-	return hash;
+	// TODO : Implement a has function...
 }
